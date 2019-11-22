647b3e15038da9752c9c0a84b509cface978128a
Helentsai <pupu.her@hotmail.com>
2019-11-22 15:06:54 -0500

    runner game using unity

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run one time 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //happen every frame per seconds
+        transform.Rotate(45.0f * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -30)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * 10.0f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * 10.0f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * 10.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * 10.0f, ForceMode.Impulse);
+        }
+    }
+}
